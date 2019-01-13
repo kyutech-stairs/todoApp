166b28373ab0b1525165125e284dfe226112d0ce
KO-YAMAKO <ko.foot1103@gmail.com>
2019-01-13 22:53:24 +0900

    update Model

--- app/src/main/kotlin/com/planningdev/todoapp/model/task.kt
+++ app/src/main/kotlin/com/planningdev/todoapp/model/task.kt
@@ -1,9 +1,12 @@
-//import io.realm.RealmObject
-////import io.realm.annotations
-////
-////open class Task (
-////    @PrimaryKey open var id: String = UUID.randomUUID().toString(),
-////    @Required open var title: String = "",
-////    @Required open var note: String = "",
-////    @Required open var createdAt: Date = Date()
-////    ) : RealmObject {}
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import io.realm.annotations.Required
+import java.util.UUID
+import java.util.Date
+
+open class Task: RealmObject() {
+    @PrimaryKey open var id: String = UUID.randomUUID().toString()
+    @Required open var title: String = ""
+    @Required open var note: String = ""
+    @Required open var createdAt: Date = Date()
+}
