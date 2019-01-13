00ff97f8620b2702da70b8793cb710ef294de8c6
KO-YAMAKO <ko.foot1103@gmail.com>
2019-01-13 17:08:32 +0900

    add new activity

---
+++ app/src/main/java/com/planningdev/todoapp/newTask.kt
@@ -0,0 +1,12 @@
+package com.planningdev.todoapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class newTask : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_task)
+    }
+}
