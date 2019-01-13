22dd41895becff241c350ab81986fad97ec90d26
KO-YAMAKO <ko.foot1103@gmail.com>
2019-01-13 18:17:09 +0900

    creating model

---
+++ app/src/main/kotlin/com/planningdev/todoapp/model/task.kt
@@ -0,0 +1,9 @@
+//import io.realm.RealmObject
+////import io.realm.annotations
+////
+////open class Task (
+////    @PrimaryKey open var id: String = UUID.randomUUID().toString(),
+////    @Required open var title: String = "",
+////    @Required open var note: String = "",
+////    @Required open var createdAt: Date = Date()
+////    ) : RealmObject {}
